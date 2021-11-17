aa9999c7c0673543f54aa8eebc5b1c25febdc6c0
Portatil <imtzayo@gmail.com>
2021-11-17 12:54:41 +0100

    changes2

--- controllers/DatuBaseKud/DBKudeatzaile.py
+++ controllers/DatuBaseKud/DBKudeatzaile.py
@@ -1,1 +1,17 @@
 import mysql.connector
+
+db = mysql.connector.connect(
+        host="localhost",
+        user= "strava",
+        password="stravapassword123",
+        database="strava"
+)
+
+kurtsorea = db.cursor()
+kurtsorea.execute("SHOW DATABASES")
+row=True
+while row:
+        row=kurtsorea.fetchall()
+        print(row)
+
+kurtsorea.execute("SHOW COLUMNS FROM Segmentuak;")
