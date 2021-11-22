17ac8796e3dadc016bb06ab9f3182b58f2aac29e
Portatil <imtzayo@gmail.com>
2021-11-22 10:04:32 +0100

    datu basea hasita

--- controllers/DatuBaseKud/DBKudeatzaile.py
+++ controllers/DatuBaseKud/DBKudeatzaile.py
@@ -8,10 +8,23 @@ db = mysql.connector.connect(
 )
 
 kurtsorea = db.cursor()
-kurtsorea.execute("SHOW DATABASES")
+
+
+if kurtsorea.execute("SHOW TABLES;"):
+        kurtsorea.execute("CREATE TABLE Entrenamendu (id int(10), mota varchar(50), dataEguna date, iraupena time, kmkop float(3),bestelakoDatuak varchar(100), PRIMARY KEY(id));")
+        kurtsorea.execute("CREATE TABLE Segmentuak (izen varchar(30), hasi time, amaitu time, PRIMARY KEY(izen));")
+        kurtsorea.execute("CREATE TABLE Ekipamendua (izen varchar(30), zapatilak varchar(30), erlojua varchar(30), bestelakoak varchar(100), PRIMARY KEY(izen));")
+        kurtsorea.execute("CREATE TABLE Medizioak (segundua time, posizioa varchar(50), abiadura int(3), pultsazioak int(3), bestelakoak varchar(50), entrenamenduId int(10),FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id));")
+        kurtsorea.execute("CREATE TABLE Jarraitzaile (nickname varchar(30), PRIMARY KEY(nickname));")
+        kurtsorea.execute("CREATE TABLE Buelta (izen varchar(30), mota varchar(30), km float(3), denbora time,pultsazioak int(3), abiadura int(3), entrenamenduId int(10),FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id));")
+        kurtsorea.execute("CREATE TABLE Kudo (jarraitzaileNickname varchar(30), entrenamenduId int(10), FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id), FOREIGN KEY(jarraitzaileNickname) REFERENCES Jarraitzaile (nickname));")
+        kurtsorea.execute("CREATE TABLE Iruzkin (iruzkina varchar(100), jarraitzaileNickname varchar(30), entrenamenduId int(10), FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id), FOREIGN KEY(jarraitzaileNickname) REFERENCES Jarraitzaile (nickname));")
+        kurtsorea.execute("CREATE TABLE Informazioa (denbora time, segmentuIzen varchar(30), entrenamenduId int(10));")
+        kurtsorea.execute("CREATE TABLE Erabili (ekipamenduIzen varchar(30), entrenamenduId int(10), FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id), FOREIGN KEY(ekipamenduIzen) REFERENCES Ekipamendua (izen));")
+
+
 row=True
 while row:
         row=kurtsorea.fetchall()
         print(row)
-
 kurtsorea.execute("SHOW COLUMNS FROM Segmentuak;")
