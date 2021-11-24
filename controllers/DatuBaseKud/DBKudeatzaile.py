99124a130de9dae8f28d5a721193b276968fd659
Portatil <imtzayo@gmail.com>
2021-11-24 12:57:10 +0100

    atxitu taulak

--- controllers/DatuBaseKud/DBKudeatzaile.py
+++ controllers/DatuBaseKud/DBKudeatzaile.py
@@ -12,24 +12,37 @@ def konexioa():
 
 
 def taulakSortu(db):
-        kurtsorea = db.cursor()
+        kurtsorea = db.cursor(buffered=True)
 
-        if kurtsorea.execute("SHOW TABLES;"):
-                kurtsorea.execute("CREATE TABLE Entrenamendu (id int(10), mota varchar(50), dataEguna date, iraupena time, kmkop float(3),bestelakoDatuak varchar(100), PRIMARY KEY(id));")
-                kurtsorea.execute("CREATE TABLE Segmentuak (izen varchar(30), hasi time, amaitu time, PRIMARY KEY(izen));")
-                kurtsorea.execute("CREATE TABLE Ekipamendua (izen varchar(30), zapatilak varchar(30), erlojua varchar(30), bestelakoak varchar(100), PRIMARY KEY(izen));")
-                kurtsorea.execute("CREATE TABLE Medizioak (segundua time, posizioa varchar(50), abiadura int(3), pultsazioak int(3), bestelakoak varchar(50), entrenamenduId int(10),FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id));")
-                kurtsorea.execute("CREATE TABLE Jarraitzaile (nickname varchar(30), PRIMARY KEY(nickname));")
-                kurtsorea.execute("CREATE TABLE Buelta (izen varchar(30), mota varchar(30), km float(3), denbora time,pultsazioak int(3), abiadura int(3), entrenamenduId int(10),FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id));")
-                kurtsorea.execute("CREATE TABLE Kudo (jarraitzaileNickname varchar(30), entrenamenduId int(10), FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id), FOREIGN KEY(jarraitzaileNickname) REFERENCES Jarraitzaile (nickname));")
-                kurtsorea.execute("CREATE TABLE Iruzkin (iruzkina varchar(100), jarraitzaileNickname varchar(30), entrenamenduId int(10), FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id), FOREIGN KEY(jarraitzaileNickname) REFERENCES Jarraitzaile (nickname));")
-                kurtsorea.execute("CREATE TABLE Informazioa (denbora time, segmentuIzen varchar(30), entrenamenduId int(10));")
-                kurtsorea.execute("CREATE TABLE Erabili (ekipamenduIzen varchar(30), entrenamenduId int(10), FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id), FOREIGN KEY(ekipamenduIzen) REFERENCES Ekipamendua (izen));")
+        kurtsorea.execute("CREATE TABLE IF NOT EXISTS Entrenamendu (id int(10), mota varchar(50), dataEguna date, iraupena time, kmkop float(3),bestelakoDatuak varchar(100), PRIMARY KEY(id));")
+        kurtsorea.execute("CREATE TABLE IF NOT EXISTS Segmentuak (izen varchar(30), hasi time, amaitu time, PRIMARY KEY(izen));")
+        kurtsorea.execute("CREATE TABLE IF NOT EXISTS Ekipamendua (izen varchar(30), zapatilak varchar(30), erlojua varchar(30), bestelakoak varchar(100), PRIMARY KEY(izen));")
+        kurtsorea.execute("CREATE TABLE IF NOT EXISTS Medizioak (segundua time, posizioa varchar(50), abiadura int(3), pultsazioak int(3), bestelakoak varchar(50), entrenamenduId int(10),FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id));")
+        kurtsorea.execute("CREATE TABLE IF NOT EXISTS Jarraitzaile (nickname varchar(30), PRIMARY KEY(nickname));")
+        kurtsorea.execute("CREATE TABLE IF NOT EXISTS Buelta (izen varchar(30), mota varchar(30), km float(3), denbora time,pultsazioak int(3), abiadura int(3), entrenamenduId int(10),FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id));")
+        kurtsorea.execute("CREATE TABLE IF NOT EXISTS Kudo (jarraitzaileNickname varchar(30), entrenamenduId int(10), FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id), FOREIGN KEY(jarraitzaileNickname) REFERENCES Jarraitzaile (nickname));")
+        kurtsorea.execute("CREATE TABLE IF NOT EXISTS Iruzkin (iruzkina varchar(100), jarraitzaileNickname varchar(30), entrenamenduId int(10), FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id), FOREIGN KEY(jarraitzaileNickname) REFERENCES Jarraitzaile (nickname));")
+        kurtsorea.execute("CREATE TABLE IF NOT EXISTS Informazioa (denbora time, segmentuIzen varchar(30), entrenamenduId int(10));")
+        kurtsorea.execute("CREATE TABLE IF NOT EXISTS Erabili (ekipamenduIzen varchar(30), entrenamenduId int(10), FOREIGN KEY(entrenamenduId) REFERENCES Entrenamendu (id), FOREIGN KEY(ekipamenduIzen) REFERENCES Ekipamendua (izen));")
 
-def erakutsi(db):
+def erakutsiTaulak(db):
         kurtsorea = db.cursor()
         row=True
+        lista=[]
+        kurtsorea.execute("SHOW TABLES;")
         while row:
-                row=kurtsorea.fetchall()
-                print(row)
-        kurtsorea.execute("SHOW COLUMNS FROM Segmentuak;")
+                row=kurtsorea.fetchone()
+                if row:
+                        print(row)
+                        text=row[0]
+                        lista.append(text)
+                        print(text)
+
+        for x in lista:
+                kurtsorea.execute(f"SHOW COLUMNS FROM {x};")
+
+                print(x, kurtsorea.fetchall())
+
+                kurtsorea.execute("SELECT * FROM buelta WHERE izen=%s", (x,))
+
+                print(x, kurtsorea.fetchall())
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from controllers.StravaAPI import stravaApiKud
 from controllers.DatuBaseKud import DBKudeatzaile
 
-
 if __name__ == '__main__':
     stravaApiKud.getAccessToTheAPI()
     print(stravaApiKud.getAthlete())
@@ -10,4 +9,6 @@ if __name__ == '__main__':
     id= emaitza[0]["id"]
     print(stravaApiKud.getActivityById(id))
     print(stravaApiKud.getActivityStreams(id,keys=["time","distance"]))
-
+    db = DBKudeatzaile.konexioa()
+    DBKudeatzaile.taulakSortu(db)
+    DBKudeatzaile.erakutsiTaulak(db)
